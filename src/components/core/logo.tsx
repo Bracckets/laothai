1090f5f713c714646b211377a0dcb964bf52ab2b
Bracckets <hm20031@gmail.com>
2024-11-20 12:00:49 +0300

    add logo.

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -22,9 +22,9 @@ export function Logo({ color = 'dark', emblem, height = HEIGHT, width = WIDTH }:
   let url: string;
 
   if (emblem) {
-    url = color === 'light' ? '/assets/logo-emblem.svg' : '/assets/logo-emblem--dark.svg';
+    url = color === 'light' ? '/assets/laothai-white.svg' : '/assets/laothai-black.svg';
   } else {
-    url = color === 'light' ? '/assets/logo.svg' : '/assets/logo--dark.svg';
+    url = color === 'light' ? '/assets/laothai-white.svg' : '/assets/laothai-black.svg';
   }
 
   return <Box alt="logo" component="img" height={height} src={url} width={width} />;
